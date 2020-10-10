5e2520232227d19836c077c0ade08895e8bee1ed
Dave Young <sixty7ss@gmail.com>
2020-10-10 00:28:10 -0700

    Progress on Career Opportunities and Related Programs sliders

--- js/templates/global.js
+++ js/templates/global.js
@@ -14,7 +14,6 @@ import PageAnchors from '../navigation/PageAnchors'
 import MainNavigation from '../navigation/MainNavigation'
 import TermNavigation from '../navigation/TermNavigation'
 
-const flickityArrow = 'M50,0A50,50,0,1,1,0,50,50.06,50.06,0,0,1,50,0Zm0,95.91A45.91,45.91,0,1,0,4.09,50,46,46,0,0,0,50,95.91Zm1.45-25.28a2.05,2.05,0,0,0,0-2.9L33.71,50,51.45,32.26a2,2,0,1,0-2.9-2.89L29.37,48.55a2,2,0,0,0,0,2.89L48.55,70.63a2.05,2.05,0,0,0,2.9,0ZM71.23,50a2.05,2.05,0,0,0-2-2H30.82a2,2,0,1,0,0,4.09H69.18A2,2,0,0,0,71.23,50Z';
 
 export default () => {
   // Flyout toggle functionality
@@ -80,11 +79,42 @@ export default () => {
 
   document.querySelectorAll('.section__career-opportunities .slider').forEach(element => {
     if (!element.Slider) {
-      element.Slider = new Flickity(element, {
-        arrowShape: flickityArrow,
+      const defaults = {
         wrapAround: true,
-      });
+        pageDots: true,
+        autoPlay: false,
+        adaptiveHeight: false,
+        arrowShape: 'M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z',
+        defaults: 1,
+      }
+      
+      element.Slider = new Flickity(element, defaults);
     }
   });
+  
+  // Certificate Related Programs Slider
+  // ---------------------------------------------------------------------------
 
+  document.querySelectorAll('.section__related-programs .slider').forEach(element => {
+    if (!element.Slider) {
+      const defaults = {
+        wrapAround: true,
+        autoPlay: false,
+        adaptiveHeight: false,
+        arrowShape: 'M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z',
+      }
+
+      if (window.matchMedia("(max-width: 992px)").matches) {
+        defaults['groupCells'] = 1;
+        defaults['prevNextButtons'] = false;
+        defaults['pageDots'] = true;
+      } else {
+        defaults['groupCells'] = 4;
+        defaults['prevNextButtons'] = true;
+        defaults['pageDots'] = false;
+
+      }
+      element.Slider = new Flickity(element, defaults);
+    }
+  });
 }
