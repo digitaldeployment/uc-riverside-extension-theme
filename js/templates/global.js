0609623aa5f75fbaacb9de991a228e255b76750f
Dave Young <sixty7ss@gmail.com>
2020-10-09 23:23:26 -0700

    Certificates :: Previews; Related Programs

--- js/templates/global.js
+++ js/templates/global.js
@@ -5,12 +5,16 @@ import 'bootstrap'
 import 'fg-collapsible/src/collapsible'
 import 'fg-collapsible/src/collapsible.set'
 import 'fg-collapsible/src/collapsible.tab'
+import 'flickity-fade';
+import Flickity from 'flickity';
 import Sticky from '../utils/Sticky'
 import Flyout from '../components/Flyout'
 import SmoothScroll from 'smooth-scroll'
 import PageAnchors from '../navigation/PageAnchors'
 import MainNavigation from '../navigation/MainNavigation'
 import TermNavigation from '../navigation/TermNavigation'
+
+const flickityArrow = 'M50,0A50,50,0,1,1,0,50,50.06,50.06,0,0,1,50,0Zm0,95.91A45.91,45.91,0,1,0,4.09,50,46,46,0,0,0,50,95.91Zm1.45-25.28a2.05,2.05,0,0,0,0-2.9L33.71,50,51.45,32.26a2,2,0,1,0-2.9-2.89L29.37,48.55a2,2,0,0,0,0,2.89L48.55,70.63a2.05,2.05,0,0,0,2.9,0ZM71.23,50a2.05,2.05,0,0,0-2-2H30.82a2,2,0,1,0,0,4.09H69.18A2,2,0,0,0,71.23,50Z';
 
 export default () => {
   // Flyout toggle functionality
@@ -70,4 +74,17 @@ export default () => {
       offset: 40,
     });
   }
+  
+  // Certificate Career Opportunities Slider
+  // ---------------------------------------------------------------------------
+
+  document.querySelectorAll('.section__career-opportunities .slider').forEach(element => {
+    if (!element.Slider) {
+      element.Slider = new Flickity(element, {
+        arrowShape: flickityArrow,
+        wrapAround: true,
+      });
+    }
+  });
+
 }
