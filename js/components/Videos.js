4157fb993fda94515129728260d3ec365bc2a621
Jason McWhorter <jason@digitaldeployment.com>
2020-10-13 16:36:52 -0700

    Abstract out the video play button behaviors and modal interation

--- js/components/Videos.js
+++ js/components/Videos.js
@@ -1,24 +1,17 @@
 import Flickity from 'flickity';
-import VideoModal from './VideoModal';
 
 const flickityArrow = 'M50,0A50,50,0,1,1,0,50,50.06,50.06,0,0,1,50,0Zm0,95.91A45.91,45.91,0,1,0,4.09,50,46,46,0,0,0,50,95.91Zm1.45-25.28a2.05,2.05,0,0,0,0-2.9L33.71,50,51.45,32.26a2,2,0,1,0-2.9-2.89L29.37,48.55a2,2,0,0,0,0,2.89L48.55,70.63a2.05,2.05,0,0,0,2.9,0ZM71.23,50a2.05,2.05,0,0,0-2-2H30.82a2,2,0,1,0,0,4.09H69.18A2,2,0,0,0,71.23,50Z';
 const selectors = {
   slider: '.slider',
   navigation: '.slider-nav',
-  modal: '#modal-video',
 };
 
 export default class Videos {
   constructor(element) {
     this.element = element;
     this.selectors = selectors;
-    this.Modal = this.addModal();
     this.Slider = this.addSlider();
     this.Navigation = this.addNavigation();
-  }
-
-  addModal() {
-    return new VideoModal(this.modal);
   }
 
   addSlider() {
@@ -26,25 +19,6 @@ export default class Videos {
       prevNextButtons: false,
       draggable: false,
       fade: true,
-    });
-
-    // Add play button click handler and callback
-    // -----------------------------------------------------------------------
-
-    Slider.cells.forEach(cell => {
-      const button = cell.element.querySelector('.button__play');
-      const videoURL = button.getAttribute('data-video-url');
-      const videoTitle = button.getAttribute('data-video-title');
-
-      if (button && this.modal && videoURL && videoTitle) {
-        // Add a click handler for the play button which overlays each
-        // video background image and title.
-        button.addEventListener('click', () => {
-          // Update modal data attributes relavent to the new video.
-          this.modal.setAttribute('data-iframe-src', videoURL);
-          this.modal.setAttribute('aria-label', videoTitle);
-        });
-      }
     });
 
     // Make sure the nav group state is relative to the active slider slide.
@@ -78,10 +52,6 @@ export default class Videos {
     });
 
     return Navigation;
-  }
-
-  get modal() {
-    return document.querySelector(this.selectors.modal);
   }
 
   get slider() {
